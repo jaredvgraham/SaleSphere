7508d85873dcd3d5a9e05e9980a3a8eacb2ad9f2
JaredvGraham <jgram153@icloud.com>
2024-10-02 19:39:50 -0400

    company id page with related

--- src/app/dashboard/[companyId]/page.tsx
+++ src/app/dashboard/[companyId]/page.tsx
@@ -8,6 +8,8 @@ const CompanyPage = () => {
   const { companyId } = useParams();
   const authFetch = useAuthFetch();
   const [company, setCompany] = useState<Company>();
+  const [relatedCompanies, setRelatedCompanies] = useState<Company[]>();
+  const [nearbyCompanies, setNearbyCompanies] = useState<Company[]>();
   useEffect(() => {
     try {
       const fetchCompany = async () => {
@@ -16,6 +18,8 @@ const CompanyPage = () => {
         });
         console.log("company", res.company);
         setCompany(res.company);
+        setRelatedCompanies(res.company.relatedCompanies);
+        setNearbyCompanies(res.company.nearbyCompanies);
       };
       fetchCompany();
     } catch (error) {
@@ -26,6 +30,18 @@ const CompanyPage = () => {
     <div className="h-screen bg-white  overflow-scroll">
       <div className="bg-black">
         <h1 className="text-white">{company?.name}</h1>
+        <h2 className="text-white">Related Companies</h2>
+        <div className="grid grid-cols-3 gap-4">
+          {relatedCompanies?.map((company) => (
+            <div key={company._id}>{company.name}</div>
+          ))}
+        </div>
+        <h2 className="text-white">Nearby Companies</h2>
+        <div className="grid grid-cols-3 gap-4">
+          {nearbyCompanies?.map((company) => (
+            <div key={company._id}>{company.name}</div>
+          ))}
+        </div>
       </div>
     </div>
   );
