bab84fc259cea2fcfefefe1430af85df3e02083c
evanwilson2123 <evanmwilson@gmail.com>
2024-10-04 12:05:34 -0400

    added console page

---
+++ src/app/console/[companyId]/page.tsx
@@ -0,0 +1,12 @@
+import Console from "@/components/console";
+import React from "react";
+
+const page = () => {
+  return (
+    <div>
+      <Console />
+    </div>
+  );
+};
+
+export default page;
