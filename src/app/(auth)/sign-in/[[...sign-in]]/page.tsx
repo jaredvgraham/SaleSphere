bb75627243a4c7730785b9e164efe4b9cba73e3d
JaredvGraham <jgram153@icloud.com>
2024-10-16 23:10:24 -0400

    signin -up

--- src/app/(auth)/sign-in/[[...sign-in]]/page.tsx
+++ src/app/(auth)/sign-in/[[...sign-in]]/page.tsx
@@ -1,5 +1,9 @@
 import { SignIn } from "@clerk/nextjs";
 
 export default function Page() {
-  return <SignIn />;
+  return (
+    <div className="min-h-screen  flex justify-center items-center h-16  text-white">
+      <SignIn />{" "}
+    </div>
+  );
 }
--- src/app/(auth)/sign-up/[[...sign-up]]/page.tsx
+++ src/app/(auth)/sign-up/[[...sign-up]]/page.tsx
@@ -3,5 +3,11 @@ import { SignUp } from "@clerk/nextjs";
 export default function Page() {
   console.log("at sign-up page");
 
-  return <SignUp />;
+  return (
+    <div className="min-h-screen  flex justify-center items-center h-16  text-white">
+      <div className="">
+        <SignUp />
+      </div>
+    </div>
+  );
 }
