4c1c47cd07cd927ea83cdcbe813bdb7ae3f6c0cc
Jared Graham <jaredgraham@mac-mini.mynetworksettings.com>
2024-11-14 09:59:16 -0500

    redirect login to dash

--- src/app/(auth)/sign-up/[[...sign-up]]/page.tsx
+++ src/app/(auth)/sign-up/[[...sign-up]]/page.tsx
@@ -6,7 +6,7 @@ export default function Page() {
   return (
     <div className="min-h-screen  flex justify-center items-center h-16  text-white">
       <div className="">
-        <SignUp forceRedirectUrl="/pricing" />
+        <SignUp forceRedirectUrl="/" />
       </div>
     </div>
   );
